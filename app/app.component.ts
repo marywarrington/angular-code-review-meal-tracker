8258fca243d4cd88aa0a539fee04dfdb1060ae84
Mary Warrington <student@epicodus.com>
2016-03-25 07:47:32 -0700

    add app.component.ts and linked meal.model.ts with basic constructor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
+  directives: [],
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-    </div>  
+      <h1>Meal Tracker!</h1>
+      <ul><li>Here's a test meal!</li></ul>
+    </div>
   `
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
 
+    ]
+  }
 }
