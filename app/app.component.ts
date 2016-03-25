a7faf6ecc210f3ce17b77de31dd7387d5fea666b
Mary Warrington <student@epicodus.com>
2016-03-25 07:55:20 -0700

    Add meal-list and meal components, linked to app to display hard coded meals

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from 'angular2/core';
 import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
 
 @Component({
   selector: 'my-app',
-  directives: [],
+  directives: [MealListComponent],
   template: `
     <div class="container">
       <h1>Meal Tracker!</h1>
-      <ul><li>Here's a test meal!</li></ul>
+      <meal-list [mealList]="meals"></meal-list>
     </div>
   `
 })
@@ -16,7 +17,9 @@ export class AppComponent {
   public meals: Meal[];
   constructor() {
     this.meals = [
-
+      new Meal("Hamburger", "So damn tasty", 900),
+      new Meal("Oatmeal", "Pretty healthy, but filling", 300),
+      new Meal("fruit bowl", "great!", 150)
     ]
   }
 }
---
+++ app/meal.component.ts
@@ -0,0 +1,21 @@
+import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+// import { MealDetailsComponent} from './meal-details.component';
+
+@Component({
+  selector: 'meal-display',
+  inputs: ['meal'],
+  // directives: [MealDetailsComponent],
+  template: `
+    <h3 (click)="mealClicked()">{{ meal.name }}</h3>
+    <meal-details *ngIf="detailShow" [meal]="meal"></meal-details>
+  `
+})
+
+export class MealComponent {
+  public meal: Meal;
+  public detailShow: boolean = false;
+  mealClicked(): void {
+    this.detailShow = !this.detailShow;
+  }
+}
